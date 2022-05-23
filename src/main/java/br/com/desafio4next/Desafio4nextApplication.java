8d8efa3f1fdde51b1c3fe3d7c33a0b635bdeabb5
felipe0059 <felipe.nx4@gmail.com>
2022-05-23 17:09:45 -0300

    Subindo projeto inicial

---
+++ src/main/java/br/com/desafio4next/Desafio4nextApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafio4next;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio4nextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio4nextApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/desafio4next/Desafio4nextApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio4next;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio4nextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
